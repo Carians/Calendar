9932fa742d5080fb5b38175032b2e5f4b66acf10
Carians <37939283+Carians@users.noreply.github.com>
2023-03-09 15:13:44 +0100

    Created Event, Calendar model and registered them in admin site

--- cal/main/models.py
+++ cal/main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    date_created = models.DateTimeField(default=timezone.now)
+    date_modified = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    def __str__(self):
+        return self.name
+
+class Calendar(models.Model):
+    name = models.CharField(max_length=200, unique=True, blank=False, null=False)
+    description = models.TextField()
+    date_created = models.DateTimeField()
+    date_modified = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    events = models.ManyToManyField(Event)
+    def __str__(self):
+        return self.name
