ed19972a1ecced9f6ff36e565c9b691db5d5af4d
RezyserKinaAkcji <michalpilarski17@gmail.com>
2023-03-09 17:25:56 +0100

    Header made with reactstrap

--- cal/frontend/calendar/src/App.js
+++ cal/frontend/calendar/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Button } from 'reactstrap';
+
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Button color='primary'> Primary </Button>
+      <Header/>
     </div>
   );
 }
