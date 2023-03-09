171dfcdb5184378fe3c28fab92747936a02cf97f
RezyserKinaAkcji <michalpilarski17@gmail.com>
2023-03-09 15:35:28 +0100

    Created React App and added reactstrap

---
+++ cal/frontend/calendar/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button } from 'reactstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Button color='primary'> Primary </Button>
+    </div>
+  );
+}
+
+export default App;
