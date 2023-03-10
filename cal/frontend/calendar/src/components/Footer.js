3ee52d613508b636a65578ec2ec7b421f44811f6
RezyserKinaAkcji <michalpilarski17@gmail.com>
2023-03-10 19:57:59 +0100

    Main and Footer added

---
+++ cal/frontend/calendar/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+export default function Footer(){
+    return(
+        <footer>
+            <p>Calendar by Michał Pilarski and Krzysztof Kasperek</p>
+        </footer>
+    )
+}
