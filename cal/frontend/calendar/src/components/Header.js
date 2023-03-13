d4c445d3e324fb166e4c0ca99675422d811b6eb2
RezyserKinaAkcji <michalpilarski17@gmail.com>
2023-03-13 21:31:57 +0100

    Fancy main page styling

--- cal/frontend/calendar/src/components/Header.js
+++ cal/frontend/calendar/src/components/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { List } from 'react-bootstrap-icons';
+import './css/Header.css'
+
+import { Calendar2Date, List } from 'react-bootstrap-icons';
 import {Container, Col, Row} from 'reactstrap'
 
 
@@ -7,10 +9,11 @@ export default function Header(){
     return(
         <header>
             <Row className="header border border-dark">
-                <Col>
+                <Col className="d-flex justify-content-center align-items-center">
+                    <div className="me-3 mb-2"><Calendar2Date size={45}/></div>
                     <h1>Calendar</h1>
                 </Col>
-                <Col className="d-flex justify-content-end pe-5">
+                <Col className="d-flex justify-content-end align-items-center pe-5">
                     <List size={52}/>
                 </Col>
             </Row>
--- cal/frontend/calendar/src/components/Main.js
+++ cal/frontend/calendar/src/components/Main.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './css/Main.css'
 
 import {Container, Col, Row} from 'reactstrap'
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
@@ -22,7 +23,7 @@ export default function Main(){
                     <Label className="me-2" for="inputPassword">Password</Label>
                     <Input type="password" name="password" placeholder="Enter password"/>
                 </FormGroup>
-                <Button>Submit</Button>
+                <Button className="submit-btn bg-primary">Submit</Button>
             </Form>
 
         </div>
