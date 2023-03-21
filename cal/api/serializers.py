8786ffc2be968797f783fe310489f2dad771182c
Carians <37939283+Carians@users.noreply.github.com>
2023-03-21 17:53:54 +0100

    Added ability to register user by API

--- cal/api/serializers.py
+++ cal/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from main.models import Calendar, Event
+from django.contrib.auth.models import User
 
 class CalendarSerializer(serializers.ModelSerializer):
 
@@ -12,3 +13,15 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password',
+            'first_name',
+            'last_name'
+        ]
