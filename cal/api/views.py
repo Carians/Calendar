d500511dcfc702b3e43473b2b0c62e06832e6999
Carians <37939283+Carians@users.noreply.github.com>
2023-04-20 20:55:40 +0200

    Fixed bug with not encrypting password after send through api

--- cal/api/views.py
+++ cal/api/views.py
@@ -105,9 +105,7 @@ class RegisterView(generics.CreateAPIView):
         if not self.request.data['last_name']:
             raise serializers.ValidationError('Last name is required')
 
-        self.request.data['password'] = make_password(self.request.data['password'])
-
-        serializer.save()
+        serializer.save(password=make_password(self.request.data['password']))
 
 
 
