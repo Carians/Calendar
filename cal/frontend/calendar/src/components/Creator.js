62bcb5e04e87f12a52980bd213ebb6585c409dd0
RezyserKinaAkcji <michalpilarski17@gmail.com>
2023-05-04 00:51:25 +0200

    Basic add event

--- cal/frontend/calendar/src/components/Creator.js
+++ cal/frontend/calendar/src/components/Creator.js
@@ -2,36 +2,69 @@ import React, { useState } from "react";
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from "@fullcalendar/interaction"
+import DatePicker from 'react-datepicker'
 
 import './css/Creator.css'
-// TODO zamienić na reactstrap
 import { Modal, Button } from "react-bootstrap";
-import { Form, FormGroup, Input, FormText  } from "reactstrap";
+import { Form, FormGroup, Input, Label } from "reactstrap";
+import "react-datepicker/dist/react-datepicker.css";
 
 
 export default function Creator(){
 
     const [modal, setModal] = useState(false);
-    // TODO zrobić handleAddEvent aby dane dodawały się do stanu event, oraz sprawdzić react-datepicker do wyboru daty w modalu
-    const [event, setEvent] = useState({})
+    const [event, setEvent] = useState({title: '', description: '', start: '', end: ''})
+    const [events, setEvents] = useState([])
+
     
     function dateClick(info){
-        setModal(prevVal => !prevVal)
+        setModal(true)
+        console.log(event)
+
+        const date = new Date(info.date)
+        date.setDate(info.date.getDate()+1)
+        setEvent({title: '', description: '', start: info.date, end: date})
+
     }
 
-    function handleAddEvent(event){
-        event.preventDefault()
+    function eventChange(val, key){
+        setEvent(prevForm =>{
+            return{
+                ...prevForm,
+                [key]: val
+            }
+        })
+    }
+
+    function handleAddEvent(ev){
+        setEvents(prevArray => [...prevArray, event])
+        console.log(events)
+        ev.preventDefault()
+    }
+
+    function renderEventContent(eventInfo){
+        return(
+            <div className="border">
+                <div className="d-flex justify-content-between align-items-start flex-row">
+                    <h4>{eventInfo.event.title}</h4>
+                    <b>{eventInfo.timeText}</b>
+                </div>
+                <div>
+                    <p className="fs-6">{eventInfo.event.extendedProps.description}</p>
+                </div>
+            </div>
+        )
     }
     
 
     return(
         <>
             <div className="d-flex justify-content-start align-items-center flex-column ps-5">
-                <form>
-                    <input type="text"/>
-                </form>
+                <Form>
+                    <Input type="text"/>
+                </Form>
             </div>  
-            <div className="fc-button">
+            <div>
                 <FullCalendar
                     plugins={[ dayGridPlugin, interactionPlugin ]}
                     initialView="dayGridMonth"
@@ -44,49 +77,42 @@ export default function Creator(){
                         end: 'today prev,next' 
                         }}
                     eventBackgroundColor="#ff0000"
-                    events={[
-                        {
-                            title: "Event 1",
-                            start: "2023-05-01",
-                            end: "2023-05-03",
-                        },
-                        {
-                            title: "Event 2",
-                            start: "2023-05-05",
-                            end: "2023-05-07",
-                            color: "blue",
-                        },
-                        {
-                        title: 'Moje wydarzenie',
-                        start: '2023-05-10T10:30:00',
-                        end: '2023-05-10T12:00:00'
-                        },
-                        {
-                        title: 'Moje wydarzenie2',
-                        start: '2023-05-10T19:30:00',
-                        end: '2023-05-10T20:00:00'
-                        },
-                    ]}
+                    events={events}
+                    eventContent={renderEventContent}
                     />
             </div>
 
-            <Modal show={modal} onHide={dateClick}>
+            <Modal show={modal} onHide={()=>{setModal(false)}}>
                 <Modal.Header closeButton>
                     <Modal.Title>Dodaj wydarzenie</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Form onSubmit={handleAddEvent} className="d-flex justify-content-center align-items-center flex-column">
-                        <FormGroup>
-                            <Input type="text" placeholder="Nazwa"/>
+                        <FormGroup className="d-flex align-items-center flex-row">
+                            <Label className="me-3">Nazwa</Label>
+                            <Input type="text" placeholder="Nazwa" onChange={title => eventChange(title.target.value, 'title')}/>
                         </FormGroup>
-                        <FormGroup>
-                            <Input type="textarea" placeholder="Opis"/>
+                        <FormGroup className="d-flex align-items-center flex-row">
+                            <Label className="me-3">Opis</Label>
+                            <Input type="textarea" placeholder="Opis" onChange={description => eventChange(description.target.value, 'description')}/>
                         </FormGroup>
-                        <FormGroup>
-                            <Input type="text" placeholder="Data rozpoczęcia"/>
+                        <FormGroup className="d-flex align-items-center flex-row">
+                            <Label className="me-3">Data rozpoczęcia</Label>
+                            <DatePicker
+                                selected={event.start}
+                                onChange={date => eventChange(date, 'start')}
+                                showTimeSelect
+                                dateFormat="MMMM d, yyyy h:mm aa"
+                            />
                         </FormGroup>
-                        <FormGroup>
-                            <Input type="text" placeholder="Data zakończenia"/>
+                        <FormGroup className="d-flex align-items-center flex-row">
+                            <Label className="me-3">Data zakończenia</Label>
+                            <DatePicker
+                                selected={event.end}
+                                onChange={date => eventChange(date, 'end')}
+                                showTimeSelect
+                                dateFormat="MMMM d, yyyy h:mm aa"
+                            />
                         </FormGroup>
                     </Form>
                 </Modal.Body>
